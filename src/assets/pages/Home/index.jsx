b4167604a77b26ea5095db68e2cbf55e1c053099
Endrick <ehpb2014@gmail.com>
2025-03-06 16:49:08 -0300

    botao Whatsapp

--- src/assets/pages/Home/index.jsx
+++ src/assets/pages/Home/index.jsx
@@ -1,7 +1,10 @@
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
+import Whatsapp from './img/whatsapp.png'
+import './styles.css'
 import Section1 from './Section1';
 import Section2 from './Section2';
+import { Link } from 'react-router-dom';
 
 function Home(){
     return(
@@ -9,6 +12,7 @@ function Home(){
             <Header/>
                 <Section1/>
                 <Section2/>
+                <Link to="https://web.whatsapp.com" id="btn_whatsapp"><img src={Whatsapp}/></Link>
             <Footer/>
         </>
     )
