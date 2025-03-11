482186608ad6acfe0ce29995c896bef16e93d1f5
Endrick <ehpb2014@gmail.com>
2025-03-11 10:52:59 -0300

    Editada a pagina sobre

--- src/assets/pages/Sobre/Section1/index.jsx
+++ src/assets/pages/Sobre/Section1/index.jsx
@@ -4,24 +4,36 @@ import logo from '../../../images/logo-horizontal-transparente.png'
 function Section1(){
     return(
         <section id="sobre">
-        <div>
-            <h2>Sobre a Mentorize</h2>
             <div className="imagemMentorize">
                 <img src={logo} alt="logotipo da mentorize" />
             </div>
-            <p>A Mentorize é a plataforma ideal para instrutores que desejam gerenciar seus cursos de forma simples e eficiente. Criado para facilitar a vida de educadores, ele permite o gerenciamento completo de turmas, conteúdos e finanças em um só lugar.</p>
-            <h3>Quem somos?</h3>
-            <p>O Mentorize foi criado por apaixonados por tecnologia e educação. Nossa equipe acredita que o ensino deve ser acessível e bem organizado, e é por isso que desenvolvemos essa plataforma para ajudar instrutores a focarem no que realmente importa: ensinar.</p>
-            <h3>Nossa missão</h3>
-            <p>Nossa missão é tornar a gestão de cursos acessível para todos os instrutores, oferecendo ferramentas poderosas para criar, organizar e expandir seus conteúdos.</p>
-            <h3>Nossos valores</h3>
-            <ul>
-                <li><strong>Inovação</strong> - Sempre buscando aprimorar a experiência educacional.</li>
-                <li><strong>Acessibilidade</strong> - Uma plataforma intuitiva para todos.</li>
-                <li><strong>Compromisso</strong> - Suporte e melhorias contínuas.</li>
-                <li><strong>Transparência</strong> - Relações honestas com nossos usuários.</li>
-            </ul>
-        </div>
+            <div className='content'>
+                <div>
+                    <h1>Sobre Nós</h1>
+                    <hr className='titulo'/>
+                    <p>A Mentorize é a plataforma ideal para instrutores que desejam gerenciar seus cursos de forma simples e eficiente. Criado para facilitar a vida de educadores, ele permite o gerenciamento completo de turmas, conteúdos e finanças em um só lugar.</p>
+                </div>
+                <div>
+                    <h3>Quem somos?</h3>
+                    <hr />
+                    <p>O Mentorize foi criado por apaixonados por tecnologia e educação. Nossa equipe acredita que o ensino deve ser acessível e bem organizado, e é por isso que desenvolvemos essa plataforma para ajudar instrutores a focarem no que realmente importa: ensinar.</p>
+                </div>
+                <div>
+                    <h3>Nossa missão</h3>
+                    <hr />
+                    <p>Nossa missão é tornar a gestão de cursos acessível para todos os instrutores, oferecendo ferramentas poderosas para criar, organizar e expandir seus conteúdos.</p>
+                </div>
+                <div>
+                    <h3>Nossos valores</h3>
+                    <hr />
+                    <ul>
+                        <li><i className="bi bi-lightbulb-fill"></i><strong>Inovação</strong> - Sempre buscando aprimorar a experiência educacional.</li>
+                        <li><i className="bi bi-bullseye"></i><strong>Acessibilidade</strong> - Uma plataforma intuitiva para todos.</li>
+                        <li><i className="bi bi-patch-check-fill"></i><strong>Compromisso</strong> - Suporte e melhorias contínuas.</li>
+                        <li><i className="bi bi-eye-fill"></i><strong>Transparência</strong> - Relações honestas com nossos usuários.</li>
+                    </ul>
+                </div>
+            </div>
     </section>
     )
 }
