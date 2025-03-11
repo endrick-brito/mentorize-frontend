e7036567ae78874ad0055cac929e822b16e3a204
Endrick <ehpb2014@gmail.com>
2025-03-11 15:43:36 -0300

    correção da importação inutilizada na pagina home

--- src/assets/pages/Home/index.jsx
+++ src/assets/pages/Home/index.jsx
@@ -3,7 +3,6 @@ import Header from '../../components/Header';
 import './styles.css'
 import Section1 from './Section1';
 import Section2 from './Section2';
-import { Link } from 'react-router-dom';
 import Whatsapp from '../../components/Whatsapp';
 
 function Home(){
