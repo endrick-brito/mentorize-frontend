bc619a7c4ee83622e56f123ab743d8aaf07e6260
Endrick <ehpb2014@gmail.com>
2025-03-13 16:46:15 -0300

    Avanço no painel de registro

--- src/assets/pages/Registro/index.jsx
+++ src/assets/pages/Registro/index.jsx
@@ -1,12 +1,21 @@
 import styles from './Registro.module.css';
 import { Link } from 'react-router-dom';
 import Botao from '../../components/Botao'
+import Logo from '../../images/logo-horizontal-transparente.png'
 
 function Registro() {
     return (
         <section className={styles.registro}>
             <div className={styles.esquerda}>
-                <Botao text={<i className="bi bi-arrow-left"></i>} url="/"/>
+                <Botao text={<i className="bi bi-arrow-left"></i>} url="/" />
+                <img src={Logo} alt="" />
+                <h1>Com o Mentorize, você cria, gerencia seus cursos e finanças com facilidade.</h1>
+                <div className={styles.social}>
+                    <i className="bi bi-instagram"></i>
+                    <i className="bi bi-youtube"></i>
+                    <i className="bi bi-twitter"></i>
+                    <i className="bi bi-linkedin"></i>
+                </div>
             </div>
             <div className={styles.direita}>
                 <form>
@@ -16,17 +25,17 @@ function Registro() {
                     </div>
                     <div className={styles.campos}>
                         <div className={styles.campo}>
-                            <input type="text" id='nome' placeholder='Nome'/>
-                            <input type="text" id='sobrenome' placeholder='Sobrenome'/>
+                            <input type="text" id='nome' placeholder='Nome' />
+                            <input type="text" id='sobrenome' placeholder='Sobrenome' />
                         </div>
                         <div className={styles.campo}>
-                            <input type="email" id='email' placeholder='Email'/>
+                            <input type="email" id='email' placeholder='Email' />
                         </div>
                         <div className={styles.campo}>
-                            <input type="password" id='senha' placeholder='Senha'/>
+                            <input type="password" id='senha' placeholder='Senha' />
                         </div>
                         <div className={styles.campo}>
-                            <input type="password" id='confirma_senha' placeholder='Repita a senha'/>
+                            <input type="password" id='confirma_senha' placeholder='Repita a senha' />
                         </div>
                         <div className={styles.campo}>
                             <input type="checkbox" name="termos" id="termos" />
